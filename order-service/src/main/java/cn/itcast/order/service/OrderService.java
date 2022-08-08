05e4c64f39eca4807767b4de6f54999b42581361
ReYe0 <1299461580@qq.com>
2022-08-08 11:09:25 +0800

    ……

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -2,27 +2,18 @@ package cn.itcast.order.service;
 
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
-import cn.itcast.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
 
-    @Autowired
-    private RestTemplate restTemplate;
     @Autowired
     private OrderMapper orderMapper;
 
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
-        // TODO 2.查询用户
-        String url = "http://localhost:8081/user/" +  order.getUserId();
-        User user = restTemplate.getForObject(url, User.class);
-        // 3.封装user信息
-        order.setUser(user);
         // 4.返回
         return order;
     }
