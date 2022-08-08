a0aa667dcb5f4f711d5de95c16b3103744dd2a28
ReYe0 <1299461580@qq.com>
2022-08-08 17:52:18 +0800

    springcloud 网关 nacos 笔记还没有做

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -1,13 +1,20 @@
 package cn.itcast.order;
 
+import cn.itcast.feign.clients.UserClient;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
+//@EnableFeignClients(defaultConfiguration = DefaultFeignConfiguration.class) //开启feign的自动化
+@EnableFeignClients(clients = UserClient.class) //指定要加载的客户端
 public class OrderApplication {
 
     public static void main(String[] args) {
@@ -19,7 +26,12 @@ public class OrderApplication {
      * @return
      */
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
+//    @Bean
+//    public IRule randomRule(){ //负载均衡设置为随机
+//        return new RandomRule();
+//    }
 }
--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,11 +1,11 @@
 package cn.itcast.order.service;
 
+import cn.itcast.feign.clients.UserClient;
+import cn.itcast.feign.pojo.User;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
-import cn.itcast.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
@@ -13,17 +13,26 @@ public class OrderService {
     @Autowired
     private OrderMapper orderMapper;
 
+//    @Autowired
+//    private RestTemplate restTemplate;
+//
+//    public Order queryOrderById(Long orderId) {
+//        // 1.查询订单
+//        Order order = orderMapper.findById(orderId);
+//        // 2.利用RestTemplate发送http请求，查询用户
+////        String url = "http://localhost:8081/user/" + order.getUserId();
+//        String url = "http://userService/user/" + order.getUserId();
+//        User user = restTemplate.getForObject(url, User.class);
+//        order.setUser(user);
+//        // 4.返回
+//        return order;
+//    }
     @Autowired
-    private RestTemplate restTemplate;
-
-    public Order queryOrderById(Long orderId) {
-        // 1.查询订单
+    private UserClient userClient;
+    public Order queryOrderById(Long orderId){
         Order order = orderMapper.findById(orderId);
-        // 2.利用RestTemplate发送http请求，查询用户
-        String url = "http://localhost:8081/user/" + order.getUserId();
-        User user = restTemplate.getForObject(url, User.class);
+        User user = userClient.findById(order.getUserId());
         order.setUser(user);
-        // 4.返回
         return order;
     }
 }
